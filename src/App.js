1c546651ed37a7f036587a3d5ee558cd127cc426
+ <+>
2023-03-06 11:56:58 +0300

    added assets

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Analytics from './components/Analytics';
+import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Hero />
+      <Analytics />
       
     </div>
   );
